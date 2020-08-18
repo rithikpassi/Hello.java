490d77f7485392290d1fd365c8073b9aba1e2f90
rithikpassi <69873688+rithikpassi@users.noreply.github.com>
2020-08-18 17:01:55 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*******************************************************************************************
+* Hello.Java
+********************************************************************************************
+* Rithik Passi
+* August 17th, 2020 
+* 255 (Section C90)
+*******************************************************************************************/
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+	System.out.println("Rithik Passi");
+	System.out.println("Hello.java");
+	System.out.println("Lab 01");
+	System.out.println("CMSC 255 C90");
+	System.out.println();
+	}
+}
